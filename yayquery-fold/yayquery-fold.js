2a041891bcca8fc98ad15926850b9c95d61dff58
peol <peolanha@gmail.com>
2009-12-19 00:13:55 +0100

    Added a timePerStep option for the audio etc.

--- yayquery-fold/yayquery-fold.js
+++ yayquery-fold/yayquery-fold.js
@@ -7,24 +7,25 @@ var
 		isReady: false,
 		timerIn: null,
 		timerOut: null,
-		fadeStep: 0.15,
+		fadeStep: 0.10,
+		timePerStep: 50,
 		debug: true
 	};
 
 $(audio).bind('loadeddata', function (e) {
-	audioOpt.debug && console.log('Audio is ready');
+	audioOpt.debug && window.console && console.log('Audio is ready');
 	audioOpt.isReady = true;
 });
 
 // Set-up the page fold on our container
-$('#target').fold({ side: 'right', autoCurl: true });
+$('#target').fold({ side: 'right', autoCurl: true, startingWidth: 180, startingHeight: 180 , maxHeight: 500 });
 
 // #turn_wrapper is a element page-fold script creates
 $('#turn_wrapper').hover(
 
 	// Play/fade in to the 'maxVolume'
 	function () {
-		audioOpt.debug && console.log('Play audio');
+		audioOpt.debug && window.console && console.log('Play audio');
 		if (!audioOpt.isReady) return;
 
 		audio.volume = audioOpt.curVol = audioOpt.fadeStep;
@@ -36,9 +37,9 @@ $('#turn_wrapper').hover(
 			function setTimer() {
 				if (audioOpt.curVol < audioOpt.maxVol) {
 					audio.volume = audioOpt.curVol = audioOpt.curVol + audioOpt.fadeStep >= 1 ? 1 : audioOpt.curVol + audioOpt.fadeStep;
-					audioOpt.timerIn = setTimeout(setTimer, 150);
+					audioOpt.timerIn = setTimeout(setTimer, audioOpt.timePerStep);
 
-					audioOpt.debug && console.log('fadein', audioOpt.curVol);
+					audioOpt.debug && window.console && console.log('fadein', audioOpt.curVol);
 				}
 				else {
 					audioOpt.timerIn = null;
@@ -51,7 +52,7 @@ $('#turn_wrapper').hover(
 
 	// Pause/fade it out and reset currentTime to 0
 	function () {
-		audioOpt.debug && console.log('Stop audio');
+		audioOpt.debug && window.console && console.log('Stop audio');
 		if (!audioOpt.isReady) return;
 
 		audioOpt.curVol = audio.volume;
@@ -63,9 +64,9 @@ $('#turn_wrapper').hover(
 				if (audioOpt.curVol !== 0) {
 					// >0 || 0
 					audio.volume = audioOpt.curVol = audioOpt.curVol - audioOpt.fadeStep < 0 ? 0 : audioOpt.curVol - audioOpt.fadeStep;
-					audioOpt.timerOut = setTimeout(setTimer, 150);
+					audioOpt.timerOut = setTimeout(setTimer, audioOpt.timePerStep);
 
-					audioOpt.debug && console.log('fadeout', audioOpt.curVol);
+					audioOpt.debug && window.console && console.log('fadeout', audioOpt.curVol);
 				}
 				else {
 					audio.currentTime = 0.1;
